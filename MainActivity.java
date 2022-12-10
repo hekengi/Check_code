3c1414619f20d635529aaef9c34290878821424d
hekengi <118739381+hekengi@users.noreply.github.com>
2022-12-10 11:15:52 +0900

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,60 +1,34 @@
-package kr.ac.cnu.computer.store;
+package kr.ac.cnu.computer.check;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button mbtn_url1;
-    Button mbtn_url2;
-    Button mbtn_url3;
-    Button mbtn_url4;
-
+    private Button btnAdd;
+    private TextView tvCount;
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mbtn_url1 = findViewById(R.id.btn_url1);
-        mbtn_url2 = findViewById(R.id.btn_url2);
-        mbtn_url3 = findViewById(R.id.btn_url3);
-        mbtn_url4 = findViewById(R.id.btn_url4);
+        tvCount = findViewById(R.id.tv_count);
+        tvCount.setText(count+"");
+        btnAdd = findViewById(R.id.btn_add);
 
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                tvCount.setText(count+"");
+            }
+        });
 
-        mbtn_url1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent urlintent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://smartstore.naver.com/sincero/"));
-                startActivity(urlintent);
-            }
-        });
-        mbtn_url2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent urlintent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://greenbliss.co.kr/"));
-                startActivity(urlintent);
-            }
-        });
-        mbtn_url3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent urlintent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://onlyeco.co.kr/"));
-                startActivity(urlintent);
-            }
-        });
-        mbtn_url4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent urlintent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://aromatica.co.kr/"));
-                startActivity(urlintent);
-            }
-        });
 
     }
 }
